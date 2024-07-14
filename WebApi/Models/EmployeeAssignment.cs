1d1cf4cc33f438424265a807908c42b73071a1a2
jasontermine <jason.termine@wiss-edu.ch>
2024-07-14 10:08:56 +0200

    ADDED: Assignment and EmployeeAssignment models

---
+++ WebApi/Models/EmployeeAssignment.cs
@@ -0,0 +1,33 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace WebApi.Models
+{
+    public class EmployeeAssignment
+    {
+        [Key]    
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [JsonIgnore]
+        public Guid uuid { get; set; }
+        
+        [Required]
+        [ForeignKey("Employee")]
+        public Guid EmployeeUuid { get; set; }
+
+        [JsonIgnore]
+        public Employee Employee { get; set; }
+
+        [Required]
+        [ForeignKey("Assignment")]
+        public Guid AssignmentUuid { get; set; }
+
+        [JsonIgnore]
+        public Assignment Assignment { get; set; }
+
+        [Required]
+        [Range(0, 999.99)]
+        public float HoursWorked { get; set; } 
+    }
+}
