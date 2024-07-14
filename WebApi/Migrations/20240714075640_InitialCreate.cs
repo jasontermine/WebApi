3fc871b1ef49b42272b2ed8f1f5ded28a76f52c4
jasontermine <jason.termine@wiss-edu.ch>
2024-07-14 10:10:34 +0200

    migrations

---
+++ WebApi/Migrations/20240714075640_InitialCreate.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Assignments",
+                columns: table => new
+                {
+                    uuid = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    startDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    dueDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Assignments", x => x.uuid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    uuid = table.Column<Guid>(type: "uuid", nullable: false),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    Age = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.uuid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeAssignments",
+                columns: table => new
+                {
+                    uuid = table.Column<Guid>(type: "uuid", nullable: false),
+                    EmployeeUuid = table.Column<Guid>(type: "uuid", nullable: false),
+                    AssignmentUuid = table.Column<Guid>(type: "uuid", nullable: false),
+                    HoursWorked = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeAssignments", x => x.uuid);
+                    table.ForeignKey(
+                        name: "FK_EmployeeAssignments_Assignments_AssignmentUuid",
+                        column: x => x.AssignmentUuid,
+                        principalTable: "Assignments",
+                        principalColumn: "uuid",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EmployeeAssignments_Employees_EmployeeUuid",
+                        column: x => x.EmployeeUuid,
+                        principalTable: "Employees",
+                        principalColumn: "uuid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeAssignments_AssignmentUuid",
+                table: "EmployeeAssignments",
+                column: "AssignmentUuid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeAssignments_EmployeeUuid",
+                table: "EmployeeAssignments",
+                column: "EmployeeUuid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeAssignments");
+
+            migrationBuilder.DropTable(
+                name: "Assignments");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
